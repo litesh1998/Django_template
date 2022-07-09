83032fa86b434c499d69b66bfacca59ef8e85bcd
Litesh Garg <49069637+litesh1998@users.noreply.github.com>
2022-07-09 15:58:17 +0530

    added django toolbar

--- {{ project_name }}/settings/local.py
+++ {{ project_name }}/settings/local.py
@@ -1,1 +1,10 @@
 from .base import *
+
+INSTALLED_APPS += ["debug_toolbar"]
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"] 
+
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
