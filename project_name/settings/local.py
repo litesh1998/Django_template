c61548f8e8c25cdab846d2f06121fcb7b20bf87e
Litesh Garg <49069637+litesh1998@users.noreply.github.com>
2022-10-16 19:35:26 +0530

    corrected debug toolbar settings

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -1,4 +1,5 @@
 from .base import *
+import socket
 
 INSTALLED_APPS += ["debug_toolbar"]
 
@@ -8,3 +9,8 @@ DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
